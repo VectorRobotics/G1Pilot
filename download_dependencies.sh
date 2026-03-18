f566c0654cfe91aecd108e179c35668778e44e1c
Kushal Agarwal <98091396+kushal-a@users.noreply.github.com>
2026-03-18 02:08:29 -0400

    Add LIBRARY_PATH to environment variables

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -13,4 +13,4 @@ export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH # Adapt your desired python version here
 export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
-
+export LIBRARY_PATH=/opt/openrobots/lib:$LIBRARY_PATH
