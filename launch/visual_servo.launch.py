989a200c19a373e6607d37104a650eaf23b93991
kushal-a <kushalagarwal444@gmail.com>
2026-03-31 17:02:26 -0400

    implementing tf look up as a core functionality

--- launch/visual_servo.launch.py
+++ launch/visual_servo.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from launch.conditions import IfCondition
@@ -40,13 +40,14 @@ def generate_launch_description():
 
     ld.add_action(DeclareLaunchArgument('namespace', default_value=''))
     ld.add_action(DeclareLaunchArgument(
-        'PublishTF', default_value='true',
+        'PublishTF', default_value='false',
         description='Whether to include display.launch.py for TF publishing'
     ))
-    ld.add_action(DeclareLaunchArgument('goal_pose_topic', default_value='arml/goal_pose'))
+    ld.add_action(DeclareLaunchArgument('goal_pose_topic', default_value='arm/goal_pose'))
     ld.add_action(DeclareLaunchArgument('feedback_topic', default_value='feedback'))
     ld.add_action(DeclareLaunchArgument('position_control_topic', default_value='position_control'))
     ld.add_action(DeclareLaunchArgument('traj_topic', default_value='traj'))
+    ld.add_action(DeclareLaunchArgument('goal_cooldown', default_value='1.0'))
 
     ld.add_action(IncludeLaunchDescription(
         PathJoinSubstitution([package_path, 'launch', 'display.launch.py']),
@@ -64,6 +65,10 @@ def generate_launch_description():
             'feedback_topic': LaunchConfiguration('feedback_topic'),
             'position_control_topic': LaunchConfiguration('position_control_topic'),
             'traj_topic': LaunchConfiguration('traj_topic'),
+            'goal_cooldown': LaunchConfiguration('goal_cooldown'),
         }],
+        remappings=[
+            (PythonExpression(["'/", LaunchConfiguration('goal_pose_topic'), "/right'"]), '/track3d/selected_normal_pose_filtered_upright')
+        ]
     ))
     return ld
